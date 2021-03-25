91439e82f3101965f2158471dad6c81d5e7a0bad
vova-rsk <77401737+vova-rsk@users.noreply.github.com>
2021-03-25 09:22:08 +0000

    Deploying to gh-pages from @ vova-rsk/barber-shop@93747c55dc37b5691444c696f8cad876d7f42ca5 🚀

--- src.61f87c08.js
+++ src.61f87c08.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/barber-shop-notfork/src.61f87c08.js.map
+//# sourceMappingURL=/barber-shop/src.61f87c08.js.map
